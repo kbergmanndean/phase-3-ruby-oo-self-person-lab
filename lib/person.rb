1bed8e0ce8966ed60e964a6df2f97b296c2efa7b
Kaia <kbergmanndean@gmail.com>
2021-06-29 17:21:00 -0400

    commit tree 2eefe7092e7bb092274267b4c479bb920e3b565a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,58 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+        
+    def initialize(name)
+        @name= name
+        @bank_account= 25
+        @happiness=8
+        @hygiene=8
+    end
+
+    def happiness=(happiness)
+    if happiness > 10 
+        @happiness = 10
+    elsif
+        happiness < 0
+        @happiness = 0
+    else
+        @happiness = happiness
+    end
+end
+
+    def hygiene=(hygiene)
+    if hygiene > 10 
+        @hygiene = 10
+    elsif
+        hygiene < 0
+        @hygiene = 0
+    else
+        @hygiene = hygiene
+    end
+end
+
+        def clean?
+            if @hygiene > 7
+                return true
+            else
+                return false
+            end
+        end
+
+        def happy?
+            if @happiness> 7
+                return true
+            else
+                return false
+            end
+        end
+
+        def get_paid(salary)
+            @bank_account += salary
+            return "all about the benjamins"
+        end
+        
+        def take_bath(song)
+            @hygiene+=4
+        end
+end
